566feea47084adf0bd1a9f8616c27276f7f20a98
hrach <hrach.cz@gmail.com>
2013-03-05 00:47:13 +0100

    Added paginator demo

---
+++ app/DatagridModule/presenters/PaginatorPresenter.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Nextras\Demos\Datagrid;
+
+use Nette;
+use Nette\Utils\Paginator;
+use Nextras;
+use Nextras\Demos\VisualPaginator;
+
+final class PaginatorPresenter extends BasePresenter
+{
+
+	/** @var Nette\Database\Connection */
+	private $connection;
+
+	public function injectConnection(Nette\Database\Connection $connection)
+	{
+		$this->connection = $connection;
+	}
+
+	public function createComponentDatagrid()
+	{
+		$grid = new Nextras\Datagrid\Datagrid;
+		$grid->addColumn('id');
+		$grid->addColumn('firstname');
+		$grid->addColumn('surname');
+		$grid->addColumn('birthday');
+
+		$grid->setDataSourceCallback($this->getDataSource);
+		$grid->setPagination(25, $this->getDataSourceSum);
+		$grid->addCellsTemplate(getNextrasDemosSource('datagrid/bootstrap-style/@bootstrap3.datagrid.latte'));
+		$grid->addCellsTemplate(getNextrasDemosSource('datagrid/bootstrap-style/@bootstrap3.extended-pagination.datagrid.latte'));
+		return $grid;
+	}
+
+	public function getDataSource($filter, $order, Paginator $paginator)
+	{
+		$selection = $this->prepareDataSource($filter, $order);
+		return $selection->limit($paginator->getItemsPerPage(), $paginator->getOffset());
+	}
+
+	public function getDataSourceSum($filter, $order)
+	{
+		return $this->prepareDataSource($filter, $order)->count('*');
+	}
+
+	private function prepareDataSource($filter, $order)
+	{
+		$filters = array();
+		foreach ($filter as $k => $v) {
+			if ($k === 'gender')
+				$filters[$k] = $v;
+			else
+				$filters[$k. ' LIKE ?'] = "%$v%";
+		}
+
+		$selection = $this->connection->table('user')->where($filters);
+		if ($order) {
+			$selection->order(implode(' ', $order));
+		}
+
+		return $selection;
+	}
+}
